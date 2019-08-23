67e541e5079bb52b07ef86c604bbacda20eadd16
Michael Reinhard <michaelreinhard@me.com>
2019-08-23 17:35:08 -0500

    changed positive to an int

--- data/process_data.py
+++ data/process_data.py
@@ -13,10 +13,30 @@ df["text_all"] = df.Summary.str.cat(df.Text, sep = ' . ')
 #confirm that they are all stings
 # print(type(df.text_all[0]))
 
-print(df.columns)
 #get rid of all the variables we will not be using
 df = df.drop(['Text', 'Id', 'ProductId', 'UserId',\
               'ProfileName', 'Time'], axis=1)
 
-print(df.columns)
+#create a small function that sets the threshold
+def postitive_threshold(df, threshold):
+    '''
+    Input: 
+        df: a Pandas DataFrame
+        threshold: a number between 2 and 5 
+    Sets the threshold at which a review will be coded as 
+    positive or negative. For example, if set at 4 the reviews 
+    that gave 4 or 5 stars will be positive and 3 and below negative.
+    '''
+    df.loc[df.Score >=threshold,'positive'] = int(1)
+    df.loc[df.Score <threshold,'positive'] = int(0)
+    return df
+
+#set positive threshold to 4
+df = postitive_threshold(df, 4)
+
+#change data type of positive to int
+df.positive = df.positive.apply(int)
+
+
+
 
