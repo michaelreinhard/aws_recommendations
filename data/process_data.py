fac717b4e929ffe5b103573bf5ea88bc422b780c
Michael Reinhard <michaelreinhard@me.com>
2019-08-23 16:44:20 -0500

    dropped variables

--- data/process_data.py
+++ data/process_data.py
@@ -7,4 +7,16 @@ cd = os.getcwd()
 #import the data 
 df = pd.read_csv(cd + '/data/reviews.csv')
 
-print(df.head())
+#combine all the text into one variable
+df["text_all"] = df.Summary.str.cat(df.Text, sep = ' . ')
+
+#confirm that they are all stings
+# print(type(df.text_all[0]))
+
+print(df.columns)
+#get rid of all the variables we will not be using
+df = df.drop(['Text', 'Id', 'ProductId', 'UserId',\
+              'ProfileName', 'Time'], axis=1)
+
+print(df.columns)
+
